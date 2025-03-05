8b07b064d65b5630c1b9501038d44ed2931ac46a
mokkadroid <acualula@gmail.com>
2025-03-05 17:04:36 +0100

    atoi testing & memset declaration in libft.h

--- libft.h
+++ libft.h
@@ -26,7 +26,7 @@ char	*ft_itoa(int n);
 void	*ft_memchr(const void *s, int c, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
-void	*ft_memmove(void *dest, const void *src, size_t n);
+void	*ft_memset(void *s, int c, size_t n);
 void	ft_putchar_fd(char c, int fd);
 void	ft_putendl_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
